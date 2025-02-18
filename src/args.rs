d91316099d958e112bd86efb1d2de1427cbe32a9
Colin Rofls <colin@cmyr.net>
2025-02-18 10:12:32 -0500

    Improve CLI docs

--- src/args.rs
+++ src/args.rs
@@ -2,8 +2,16 @@
 
 use std::path::PathBuf;
 
+/// Finding source repositories for Google Fonts fonts
+///
+/// This tool crawls fonts listed at <https://github.com/google/fonts> and looks
+/// for those that have repositories listed in their metadata files. It then
+/// looks for a config.yaml file in a `sources` directory for those repositories.
+///
+/// Finally it generates JSON output that list all of the repositories found
+/// that contain a config file we understand.
 #[derive(Clone, Debug, Default, clap::Parser)]
-#[command(version, about)]
+#[command(version)]
 #[doc(hidden)] // only intended to be used from our binary
 pub struct Args {
     /// Path to a directory where we will store font sources.
@@ -11,7 +19,7 @@ pub struct Args {
     /// This should be a directory dedicated to this task; the tool will
     /// assume that anything in it can be modified or deleted as needed.
     pub fonts_dir: PathBuf,
-    /// Path to write output. If omitted, output is printed to stdout
+    /// File path to write output JSON. If omitted, output is printed to stdout
     #[arg(short, long)]
     pub out: Option<PathBuf>,
     /// Just print a list of repository URLs
